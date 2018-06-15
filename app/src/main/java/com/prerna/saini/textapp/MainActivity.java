99e49dab8b39628fcd6c35a6c7d2842bcb47a0e0
Prerna <sainiprerna11@gmail.com>
2018-06-16 00:07:35 +0530

    first commit

---
+++ app/src/main/java/com/prerna/saini/textapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.prerna.saini.textapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    public void text(View view)
+    {
+        startActivity(new Intent(MainActivity.this, Activity1.class));
+    }
+    public void face(View view)
+    {
+        startActivity(new Intent(MainActivity.this, Activity2.class));
+    }
+}
